63b96f37da0eba008f57b60759f4e0f1179fd9ad
Charlie <cbeniere@tibco.com>
2020-03-23 13:14:21 +0100

    change repo_user to charliepben

--- couchpotato/core/_base/updater/main.py
+++ couchpotato/core/_base/updater/main.py
@@ -203,7 +203,7 @@ class BaseUpdater(Plugin):
 class GitUpdater(BaseUpdater):
 
     old_repo = 'RuudBurger/CouchPotatoServer'
-    new_repo = 'CouchPotato/CouchPotatoServer'
+    new_repo = 'charliepben/CouchPotatoServer'
 
     def __init__(self, git_command):
         self.repo = LocalRepository(Env.get('app_dir'), command = git_command)
